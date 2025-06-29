8094fedc74d7ae3755b343cd76d26c4e4a7ecce7
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-29 22:30:11 +0530

    initial commit

--- AgroSmartBeackend/obj/Debug/net8.0/AgroSmartBeackend.AssemblyInfo.cs
+++ AgroSmartBeackend/obj/Debug/net8.0/AgroSmartBeackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf49c74ec5deaf94766a2e200dc4904e185aba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
