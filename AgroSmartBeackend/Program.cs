dcf49c74ec5deaf94766a2e200dc4904e185aba5
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-06-29 22:27:24 +0530

    Update Program.cs

--- AgroSmartBeackend/Program.cs
+++ AgroSmartBeackend/Program.cs
@@ -1,3 +1,7 @@
+using AgroSmartBeackend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<MyDbContext>((serviceProvider, options) =>
+//{
+//    var config = serviceProvider.GetRequiredService<IConfiguration>();
+//    options.UseSqlServer(config.GetConnectionString("myConnectionString"));
+//});
+
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetService<IConfiguration>();
+builder.Services.AddDbContext<AgroSmartContext>(item => item.UseSqlServer(config.GetConnectionString("myConnectionString")));
 
 var app = builder.Build();
 
