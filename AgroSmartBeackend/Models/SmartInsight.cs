9ba3c91c6f664847788e60326238728dbeda282d
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-07-07 22:37:13 +0530

    Create SmartInsight.cs

---
+++ AgroSmartBeackend/Models/SmartInsight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
+
+namespace AgroSmartBeackend.Models;
+
+public partial class SmartInsight
+{
+    public int InsightId { get; set; }
+
+    public string InsightType { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Message { get; set; } = null!;
+
+    public string? Priority { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? SourceType { get; set; }
+
+    public int? SourceId { get; set; }
+
+    public int TargetUserId { get; set; }
+
+    public DateTime? ValidUntil { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public bool? IsResolved { get; set; }
+
+    [JsonIgnore]
+    public virtual User? TargetUser { get; set; }
+}
