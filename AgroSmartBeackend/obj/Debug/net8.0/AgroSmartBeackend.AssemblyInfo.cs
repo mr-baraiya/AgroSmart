a342b0dfe52657d859b38f1213329ba3dbc74915
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-07-07 22:38:13 +0530

    07-07-2025

--- AgroSmartBeackend/obj/Debug/net8.0/AgroSmartBeackend.AssemblyInfo.cs
+++ AgroSmartBeackend/obj/Debug/net8.0/AgroSmartBeackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bb59bba5bfcc40b83b9a30024f47b3ecec7167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51286cabb6fc7b7cf7b1970611f09fe99ee75b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgroSmartBeackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
