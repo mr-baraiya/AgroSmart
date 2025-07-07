7f338ba4201ad552a06604efde9619e4e03fd079
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-07-07 22:37:20 +0530

    Create Sensor.cs

---
+++ AgroSmartBeackend/Models/Sensor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
+
+namespace AgroSmartBeackend.Models;
+
+public partial class Sensor
+{
+    public int SensorId { get; set; }
+
+    public string SensorType { get; set; } = null!;
+
+    public string? Manufacturer { get; set; }
+
+    public string? Model { get; set; }
+
+    public string? SerialNumber { get; set; }
+
+    public int FieldId { get; set; }
+
+    public DateTime? InstallationDate { get; set; }
+
+    public DateTime? LastCalibrated { get; set; }
+
+    public int? CalibrationInterval { get; set; }
+
+    public decimal? LatestValue { get; set; }
+
+    public string? LatestUnit { get; set; }
+
+    public decimal? LatestQualityScore { get; set; }
+
+    public DateTime? LastReadingTime { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    [JsonIgnore]
+    public virtual Field? Field { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<SensorReading> SensorReadings { get; set; } = new List<SensorReading>();
+}
