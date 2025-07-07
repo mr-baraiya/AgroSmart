df5544ea20848c4262f6190cab1b8a3340ecca8a
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2025-07-07 22:37:16 +0530

    Create SensorReading.cs

---
+++ AgroSmartBeackend/Models/SensorReading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+//using Newtonsoft.Json;
+
+namespace AgroSmartBeackend.Models;
+
+public partial class SensorReading
+{
+    public long ReadingId { get; set; }
+
+    public int SensorId { get; set; }
+
+    public decimal Value { get; set; }
+
+    public string? Unit { get; set; }
+
+    public decimal? QualityScore { get; set; }
+
+    public DateTime ReadingTime { get; set; }
+
+    [JsonIgnore]
+    public virtual Sensor? Sensor { get; set; }
+}
